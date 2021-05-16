11c29c72f30edc04696f95700f028df410d823ef
ethcam50 <82426397+ethcam50@users.noreply.github.com>
2021-05-17 09:26:14 +1200

    Cycle1Day7
    Completed character movement with charctaer

--- Kung Fu Fisherman (GameProject)/Player.gd
+++ Kung Fu Fisherman (GameProject)/Player.gd
@@ -2,18 +2,25 @@ extends KinematicBody
 
 var speed = 10
 var acceleration = 5
-var mouse_sensitivity = 0.1
+var mouse_sensitivity = 0.3
 
 var direction = Vector3()
 var velocity = Vector3()
 
+onready var pivot = $Pivot
+
 func _ready():
-	pass 
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
-		
+		pivot.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg2rad(-90), deg2rad(90))
 
 func _process(delta):
 	
@@ -36,7 +43,6 @@ func _process(delta):
 		direction += transform.basis.x
 		
 	direction = direction.normalized()
-	velocity = direction * speed
-	velocity.linear_interpolate(velocity, acceleration * delta)
-	move_and_slide(velocity, Vector3.UP)
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
 	
