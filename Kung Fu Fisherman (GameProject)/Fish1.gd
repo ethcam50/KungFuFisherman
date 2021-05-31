9f1905d6d3b69f82b7fe862bda1f405a7d9b9f02
ethcam50 <82426397+ethcam50@users.noreply.github.com>
2021-05-31 14:57:31 +1200

    Cycle2Day3
    Did some more AI and fixed some of the movement

--- Kung Fu Fisherman (GameProject)/Fish1.gd
+++ Kung Fu Fisherman (GameProject)/Fish1.gd
@@ -2,14 +2,21 @@ extends KinematicBody
 
 var player
 var player_follow = false
-var move_speed = 100
+var move_speed = 300
 
 func _ready():
 	pass 
 
 func _physics_process(delta):
 	if player_follow == true:
-		pass
+		var pos = player.global_transform.origin
+		var facing = -global_transform.basis.z
+		look_at(pos, Vector3.UP)
+		rotation.x = 0
+		rotation.z = 0
+		move_and_slide(facing * move_speed * delta, Vector3.UP)
+		
+		
 
 func _on_Area_body_entered(body):
 	if body.name == "Player":
